7ee1be3649190c222d7c38428349de3a06929c1a
BrianDX <boris.zaitsev@devexpress.com>
2018-08-02 12:35:55 +0300

    Update to v18.1.5 and Data Source path

--- CS/Dashboard_UnderlyingDataWPF/SalesDashboard.Designer.cs
+++ CS/Dashboard_UnderlyingDataWPF/SalesDashboard.Designer.cs
@@ -29,78 +29,82 @@
         private void InitializeComponent()
         {
             DevExpress.DashboardCommon.Dimension dimension1 = new DevExpress.DashboardCommon.Dimension();
-            DevExpress.DashboardCommon.GridDimensionColumn gridDimensionColumn1 = new DevExpress.DashboardCommon.GridDimensionColumn();
             DevExpress.DashboardCommon.Measure measure1 = new DevExpress.DashboardCommon.Measure();
-            DevExpress.DashboardCommon.GridMeasureColumn gridMeasureColumn1 = new DevExpress.DashboardCommon.GridMeasureColumn();
             DevExpress.DashboardCommon.Dimension dimension2 = new DevExpress.DashboardCommon.Dimension();
+            DevExpress.DashboardCommon.Dimension dimension3 = new DevExpress.DashboardCommon.Dimension();
+            DevExpress.DashboardCommon.Dimension dimension4 = new DevExpress.DashboardCommon.Dimension();
             DevExpress.DashboardCommon.Measure measure2 = new DevExpress.DashboardCommon.Measure();
             DevExpress.DashboardCommon.ChartPane chartPane1 = new DevExpress.DashboardCommon.ChartPane();
             DevExpress.DashboardCommon.SimpleSeries simpleSeries1 = new DevExpress.DashboardCommon.SimpleSeries();
-            DevExpress.DashboardCommon.Dimension dimension3 = new DevExpress.DashboardCommon.Dimension();
-            DevExpress.DashboardCommon.Measure measure3 = new DevExpress.DashboardCommon.Measure();
-            DevExpress.DashboardCommon.Dimension dimension4 = new DevExpress.DashboardCommon.Dimension();
             DevExpress.DashboardCommon.Dimension dimension5 = new DevExpress.DashboardCommon.Dimension();
+            DevExpress.DashboardCommon.GridDimensionColumn gridDimensionColumn1 = new DevExpress.DashboardCommon.GridDimensionColumn();
+            DevExpress.DashboardCommon.Measure measure3 = new DevExpress.DashboardCommon.Measure();
+            DevExpress.DashboardCommon.GridMeasureColumn gridMeasureColumn1 = new DevExpress.DashboardCommon.GridMeasureColumn();
             DevExpress.DashboardCommon.DashboardLayoutGroup dashboardLayoutGroup1 = new DevExpress.DashboardCommon.DashboardLayoutGroup();
             DevExpress.DashboardCommon.DashboardLayoutItem dashboardLayoutItem1 = new DevExpress.DashboardCommon.DashboardLayoutItem();
             DevExpress.DashboardCommon.DashboardLayoutGroup dashboardLayoutGroup2 = new DevExpress.DashboardCommon.DashboardLayoutGroup();
             DevExpress.DashboardCommon.DashboardLayoutItem dashboardLayoutItem2 = new DevExpress.DashboardCommon.DashboardLayoutItem();
             DevExpress.DashboardCommon.DashboardLayoutItem dashboardLayoutItem3 = new DevExpress.DashboardCommon.DashboardLayoutItem();
-            this.gridDashboardItem1 = new DevExpress.DashboardCommon.GridDashboardItem();
+            this.pivotDashboardItem1 = new DevExpress.DashboardCommon.PivotDashboardItem();
             this.dashboardExtractDataSource1 = new DevExpress.DashboardCommon.DashboardExtractDataSource();
             this.chartDashboardItem1 = new DevExpress.DashboardCommon.ChartDashboardItem();
-            this.pivotDashboardItem1 = new DevExpress.DashboardCommon.PivotDashboardItem();
-            ((System.ComponentModel.ISupportInitialize)(this.gridDashboardItem1)).BeginInit();
+            this.gridDashboardItem1 = new DevExpress.DashboardCommon.GridDashboardItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pivotDashboardItem1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(dimension1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(measure1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(dimension2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(dimension3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dashboardExtractDataSource1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chartDashboardItem1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(dimension2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(measure2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pivotDashboardItem1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(dimension3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(measure3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(dimension4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(measure2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridDashboardItem1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(dimension5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(measure3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
             // 
-            // gridDashboardItem1
+            // pivotDashboardItem1
             // 
-            dimension1.DataMember = "SalesPerson";
-            gridDimensionColumn1.WidthType = DevExpress.DashboardCommon.GridColumnFixedWidthType.Weight;
-            gridDimensionColumn1.AddDataItem("Dimension", dimension1);
+            dimension1.DataMember = "OrderDate";
+            this.pivotDashboardItem1.Columns.AddRange(new DevExpress.DashboardCommon.Dimension[] {
+            dimension1});
+            this.pivotDashboardItem1.ComponentName = "pivotDashboardItem1";
             measure1.DataMember = "ExtendedPrice";
-            gridMeasureColumn1.WidthType = DevExpress.DashboardCommon.GridColumnFixedWidthType.Weight;
-            gridMeasureColumn1.AddDataItem("Measure", measure1);
-            this.gridDashboardItem1.Columns.AddRange(new DevExpress.DashboardCommon.GridColumnBase[] {
-            gridDimensionColumn1,
-            gridMeasureColumn1});
-            this.gridDashboardItem1.ComponentName = "gridDashboardItem1";
-            this.gridDashboardItem1.DataItemRepository.Clear();
-            this.gridDashboardItem1.DataItemRepository.Add(measure1, "DataItem1");
-            this.gridDashboardItem1.DataItemRepository.Add(dimension1, "DataItem0");
-            this.gridDashboardItem1.DataSource = this.dashboardExtractDataSource1;
-            this.gridDashboardItem1.InteractivityOptions.IgnoreMasterFilters = false;
-            this.gridDashboardItem1.Name = "Grid 1";
-            this.gridDashboardItem1.ShowCaption = false;
+            dimension2.DataMember = "CategoryName";
+            dimension3.DataMember = "ProductName";
+            this.pivotDashboardItem1.DataItemRepository.Clear();
+            this.pivotDashboardItem1.DataItemRepository.Add(measure1, "DataItem0");
+            this.pivotDashboardItem1.DataItemRepository.Add(dimension1, "DataItem1");
+            this.pivotDashboardItem1.DataItemRepository.Add(dimension2, "DataItem2");
+            this.pivotDashboardItem1.DataItemRepository.Add(dimension3, "DataItem3");
+            this.pivotDashboardItem1.DataSource = this.dashboardExtractDataSource1;
+            this.pivotDashboardItem1.InteractivityOptions.IgnoreMasterFilters = false;
+            this.pivotDashboardItem1.LayoutType = DevExpress.DashboardCommon.PivotLayoutType.Compact;
+            this.pivotDashboardItem1.Name = "Pivot 1";
+            this.pivotDashboardItem1.Rows.AddRange(new DevExpress.DashboardCommon.Dimension[] {
+            dimension2,
+            dimension3});
+            this.pivotDashboardItem1.ShowCaption = false;
+            this.pivotDashboardItem1.Values.AddRange(new DevExpress.DashboardCommon.Measure[] {
+            measure1});
             // 
             // dashboardExtractDataSource1
             // 
             this.dashboardExtractDataSource1.ComponentName = "dashboardExtractDataSource1";
-            this.dashboardExtractDataSource1.FileName = "C:\\Gallery\\WhatsNew18.1\\Dashboard\\Dashboard_UnderlyingDataWPF\\Dashboard_Underlyin" +
-    "gDataWPF\\Data\\NWind_SalesPerson.dat";
+            this.dashboardExtractDataSource1.FileName = "|DataDirectory|\\Data\\NWind_SalesPerson.dat";
             this.dashboardExtractDataSource1.Name = "Extract Data Source 1";
             // 
             // chartDashboardItem1
             // 
-            dimension2.DataMember = "OrderDate";
+            dimension4.DataMember = "OrderDate";
             this.chartDashboardItem1.Arguments.AddRange(new DevExpress.DashboardCommon.Dimension[] {
-            dimension2});
+            dimension4});
             this.chartDashboardItem1.AxisX.TitleVisible = false;
             this.chartDashboardItem1.ComponentName = "chartDashboardItem1";
             measure2.DataMember = "ExtendedPrice";
             this.chartDashboardItem1.DataItemRepository.Clear();
             this.chartDashboardItem1.DataItemRepository.Add(measure2, "DataItem0");
-            this.chartDashboardItem1.DataItemRepository.Add(dimension2, "DataItem1");
+            this.chartDashboardItem1.DataItemRepository.Add(dimension4, "DataItem1");
             this.chartDashboardItem1.DataSource = this.dashboardExtractDataSource1;
             this.chartDashboardItem1.InteractivityOptions.IgnoreMasterFilters = false;
             this.chartDashboardItem1.Name = "Chart 1";
@@ -118,30 +122,25 @@
             chartPane1});
             this.chartDashboardItem1.ShowCaption = false;
             // 
-            // pivotDashboardItem1
+            // gridDashboardItem1
             // 
-            dimension3.DataMember = "OrderDate";
-            this.pivotDashboardItem1.Columns.AddRange(new DevExpress.DashboardCommon.Dimension[] {
-            dimension3});
-            this.pivotDashboardItem1.ComponentName = "pivotDashboardItem1";
+            dimension5.DataMember = "SalesPerson";
+            gridDimensionColumn1.WidthType = DevExpress.DashboardCommon.GridColumnFixedWidthType.Weight;
+            gridDimensionColumn1.AddDataItem("Dimension", dimension5);
             measure3.DataMember = "ExtendedPrice";
-            dimension4.DataMember = "CategoryName";
-            dimension5.DataMember = "ProductName";
-            this.pivotDashboardItem1.DataItemRepository.Clear();
-            this.pivotDashboardItem1.DataItemRepository.Add(measure3, "DataItem0");
-            this.pivotDashboardItem1.DataItemRepository.Add(dimension3, "DataItem1");
-            this.pivotDashboardItem1.DataItemRepository.Add(dimension4, "DataItem2");
-            this.pivotDashboardItem1.DataItemRepository.Add(dimension5, "DataItem3");
-            this.pivotDashboardItem1.DataSource = this.dashboardExtractDataSource1;
-            this.pivotDashboardItem1.InteractivityOptions.IgnoreMasterFilters = false;
-            this.pivotDashboardItem1.LayoutType = DevExpress.DashboardCommon.PivotLayoutType.Compact;
-            this.pivotDashboardItem1.Name = "Pivot 1";
-            this.pivotDashboardItem1.Rows.AddRange(new DevExpress.DashboardCommon.Dimension[] {
-            dimension4,
-            dimension5});
-            this.pivotDashboardItem1.ShowCaption = false;
-            this.pivotDashboardItem1.Values.AddRange(new DevExpress.DashboardCommon.Measure[] {
-            measure3});
+            gridMeasureColumn1.WidthType = DevExpress.DashboardCommon.GridColumnFixedWidthType.Weight;
+            gridMeasureColumn1.AddDataItem("Measure", measure3);
+            this.gridDashboardItem1.Columns.AddRange(new DevExpress.DashboardCommon.GridColumnBase[] {
+            gridDimensionColumn1,
+            gridMeasureColumn1});
+            this.gridDashboardItem1.ComponentName = "gridDashboardItem1";
+            this.gridDashboardItem1.DataItemRepository.Clear();
+            this.gridDashboardItem1.DataItemRepository.Add(measure3, "DataItem1");
+            this.gridDashboardItem1.DataItemRepository.Add(dimension5, "DataItem0");
+            this.gridDashboardItem1.DataSource = this.dashboardExtractDataSource1;
+            this.gridDashboardItem1.InteractivityOptions.IgnoreMasterFilters = false;
+            this.gridDashboardItem1.Name = "Grid 1";
+            this.gridDashboardItem1.ShowCaption = false;
             // 
             // SalesDashboard
             // 
@@ -152,17 +151,17 @@
             this.gridDashboardItem1,
             this.chartDashboardItem1});
             dashboardLayoutItem1.DashboardItem = this.pivotDashboardItem1;
-            dashboardLayoutItem1.Weight = 49.960095770151639D;
+            dashboardLayoutItem1.Weight = 49.921752738654149D;
             dashboardLayoutItem2.DashboardItem = this.chartDashboardItem1;
-            dashboardLayoutItem2.Weight = 49.938195302843013D;
+            dashboardLayoutItem2.Weight = 49.876543209876544D;
             dashboardLayoutItem3.DashboardItem = this.gridDashboardItem1;
-            dashboardLayoutItem3.Weight = 50.061804697156987D;
+            dashboardLayoutItem3.Weight = 50.123456790123456D;
             dashboardLayoutGroup2.ChildNodes.AddRange(new DevExpress.DashboardCommon.DashboardLayoutNode[] {
             dashboardLayoutItem2,
             dashboardLayoutItem3});
             dashboardLayoutGroup2.DashboardItem = null;
             dashboardLayoutGroup2.Orientation = DevExpress.DashboardCommon.DashboardLayoutGroupOrientation.Vertical;
-            dashboardLayoutGroup2.Weight = 50.039904229848361D;
+            dashboardLayoutGroup2.Weight = 50.078247261345851D;
             dashboardLayoutGroup1.ChildNodes.AddRange(new DevExpress.DashboardCommon.DashboardLayoutNode[] {
             dashboardLayoutItem1,
             dashboardLayoutGroup2});
@@ -171,16 +170,16 @@
             this.Title.Text = "Dashboard";
             ((System.ComponentModel.ISupportInitialize)(dimension1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(measure1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.gridDashboardItem1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dashboardExtractDataSource1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(dimension2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(dimension3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pivotDashboardItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dashboardExtractDataSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(dimension4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(measure2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.chartDashboardItem1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(dimension3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(measure3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(dimension4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(dimension5)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pivotDashboardItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(measure3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridDashboardItem1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
 
         }
