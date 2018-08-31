9354e4e2b80e0cd41888c650f9bfec2b894bc174
DevExpressExampleBot <fakeEmail>
2018-08-31 17:18:01 +0300

    Source auto update [skip ci]

--- VB/Dashboard_UnderlyingDataWPF/SalesDashboard.Designer.vb
+++ VB/Dashboard_UnderlyingDataWPF/SalesDashboard.Designer.vb
@@ -26,36 +26,39 @@
             Dim dimension1 As New DevExpress.DashboardCommon.Dimension()
             Dim measure1 As New DevExpress.DashboardCommon.Measure()
             Dim dimension2 As New DevExpress.DashboardCommon.Dimension()
+            Dim calculatedField1 As New DevExpress.DashboardCommon.CalculatedField()
             Dim dimension3 As New DevExpress.DashboardCommon.Dimension()
-            Dim dimension4 As New DevExpress.DashboardCommon.Dimension()
             Dim measure2 As New DevExpress.DashboardCommon.Measure()
             Dim chartPane1 As New DevExpress.DashboardCommon.ChartPane()
             Dim simpleSeries1 As New DevExpress.DashboardCommon.SimpleSeries()
-            Dim dimension5 As New DevExpress.DashboardCommon.Dimension()
-            Dim gridDimensionColumn1 As New DevExpress.DashboardCommon.GridDimensionColumn()
             Dim measure3 As New DevExpress.DashboardCommon.Measure()
-            Dim gridMeasureColumn1 As New DevExpress.DashboardCommon.GridMeasureColumn()
+            Dim measure4 As New DevExpress.DashboardCommon.Measure()
+            Dim card1 As New DevExpress.DashboardCommon.Card()
+            Dim cardCompactLayoutTemplate1 As New DevExpress.DashboardCommon.CardCompactLayoutTemplate()
+            Dim dimension4 As New DevExpress.DashboardCommon.Dimension()
+            Dim dimension5 As New DevExpress.DashboardCommon.Dimension()
             Dim dashboardLayoutGroup1 As New DevExpress.DashboardCommon.DashboardLayoutGroup()
-            Dim dashboardLayoutItem1 As New DevExpress.DashboardCommon.DashboardLayoutItem()
             Dim dashboardLayoutGroup2 As New DevExpress.DashboardCommon.DashboardLayoutGroup()
+            Dim dashboardLayoutItem1 As New DevExpress.DashboardCommon.DashboardLayoutItem()
             Dim dashboardLayoutItem2 As New DevExpress.DashboardCommon.DashboardLayoutItem()
             Dim dashboardLayoutItem3 As New DevExpress.DashboardCommon.DashboardLayoutItem()
             Me.pivotDashboardItem1 = New DevExpress.DashboardCommon.PivotDashboardItem()
             Me.dashboardExtractDataSource1 = New DevExpress.DashboardCommon.DashboardExtractDataSource()
             Me.chartDashboardItem1 = New DevExpress.DashboardCommon.ChartDashboardItem()
-            Me.gridDashboardItem1 = New DevExpress.DashboardCommon.GridDashboardItem()
+            Me.cardDashboardItem1 = New DevExpress.DashboardCommon.CardDashboardItem()
             CType(Me.pivotDashboardItem1, System.ComponentModel.ISupportInitialize).BeginInit()
             CType(dimension1, System.ComponentModel.ISupportInitialize).BeginInit()
             CType(measure1, System.ComponentModel.ISupportInitialize).BeginInit()
             CType(dimension2, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(dimension3, System.ComponentModel.ISupportInitialize).BeginInit()
             CType(Me.dashboardExtractDataSource1, System.ComponentModel.ISupportInitialize).BeginInit()
             CType(Me.chartDashboardItem1, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(dimension4, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(dimension3, System.ComponentModel.ISupportInitialize).BeginInit()
             CType(measure2, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(Me.gridDashboardItem1, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(dimension5, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.cardDashboardItem1, System.ComponentModel.ISupportInitialize).BeginInit()
             CType(measure3, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(measure4, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(dimension4, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(dimension5, System.ComponentModel.ISupportInitialize).BeginInit()
             CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
             ' 
             ' pivotDashboardItem1
@@ -65,36 +68,37 @@
             Me.pivotDashboardItem1.ComponentName = "pivotDashboardItem1"
             measure1.DataMember = "ExtendedPrice"
             dimension2.DataMember = "CategoryName"
-            dimension3.DataMember = "ProductName"
             Me.pivotDashboardItem1.DataItemRepository.Clear()
             Me.pivotDashboardItem1.DataItemRepository.Add(measure1, "DataItem0")
             Me.pivotDashboardItem1.DataItemRepository.Add(dimension1, "DataItem1")
             Me.pivotDashboardItem1.DataItemRepository.Add(dimension2, "DataItem2")
-            Me.pivotDashboardItem1.DataItemRepository.Add(dimension3, "DataItem3")
             Me.pivotDashboardItem1.DataSource = Me.dashboardExtractDataSource1
             Me.pivotDashboardItem1.InteractivityOptions.IgnoreMasterFilters = False
             Me.pivotDashboardItem1.LayoutType = DevExpress.DashboardCommon.PivotLayoutType.Compact
             Me.pivotDashboardItem1.Name = "Pivot 1"
-            Me.pivotDashboardItem1.Rows.AddRange(New DevExpress.DashboardCommon.Dimension() { dimension2, dimension3})
+            Me.pivotDashboardItem1.Rows.AddRange(New DevExpress.DashboardCommon.Dimension() { dimension2})
             Me.pivotDashboardItem1.ShowCaption = False
             Me.pivotDashboardItem1.Values.AddRange(New DevExpress.DashboardCommon.Measure() { measure1})
             ' 
             ' dashboardExtractDataSource1
             ' 
+            calculatedField1.Expression = "[Quantity] * [UnitPrice]"
+            calculatedField1.Name = "RegularPrice"
+            Me.dashboardExtractDataSource1.CalculatedFields.AddRange(New DevExpress.DashboardCommon.CalculatedField() { calculatedField1})
             Me.dashboardExtractDataSource1.ComponentName = "dashboardExtractDataSource1"
             Me.dashboardExtractDataSource1.FileName = "|DataDirectory|\Data\NWind_SalesPerson.dat"
             Me.dashboardExtractDataSource1.Name = "Extract Data Source 1"
             ' 
             ' chartDashboardItem1
             ' 
-            dimension4.DataMember = "OrderDate"
-            Me.chartDashboardItem1.Arguments.AddRange(New DevExpress.DashboardCommon.Dimension() { dimension4})
+            dimension3.DataMember = "OrderDate"
+            Me.chartDashboardItem1.Arguments.AddRange(New DevExpress.DashboardCommon.Dimension() { dimension3})
             Me.chartDashboardItem1.AxisX.TitleVisible = False
             Me.chartDashboardItem1.ComponentName = "chartDashboardItem1"
             measure2.DataMember = "ExtendedPrice"
             Me.chartDashboardItem1.DataItemRepository.Clear()
             Me.chartDashboardItem1.DataItemRepository.Add(measure2, "DataItem0")
-            Me.chartDashboardItem1.DataItemRepository.Add(dimension4, "DataItem1")
+            Me.chartDashboardItem1.DataItemRepository.Add(dimension3, "DataItem1")
             Me.chartDashboardItem1.DataSource = Me.dashboardExtractDataSource1
             Me.chartDashboardItem1.InteractivityOptions.IgnoreMasterFilters = False
             Me.chartDashboardItem1.Name = "Chart 1"
@@ -110,54 +114,82 @@
             Me.chartDashboardItem1.Panes.AddRange(New DevExpress.DashboardCommon.ChartPane() { chartPane1})
             Me.chartDashboardItem1.ShowCaption = False
             ' 
-            ' gridDashboardItem1
+            ' cardDashboardItem1
             ' 
-            dimension5.DataMember = "SalesPerson"
-            gridDimensionColumn1.WidthType = DevExpress.DashboardCommon.GridColumnFixedWidthType.Weight
-            gridDimensionColumn1.AddDataItem("Dimension", dimension5)
             measure3.DataMember = "ExtendedPrice"
-            gridMeasureColumn1.WidthType = DevExpress.DashboardCommon.GridColumnFixedWidthType.Weight
-            gridMeasureColumn1.AddDataItem("Measure", measure3)
-            Me.gridDashboardItem1.Columns.AddRange(New DevExpress.DashboardCommon.GridColumnBase() { gridDimensionColumn1, gridMeasureColumn1})
-            Me.gridDashboardItem1.ComponentName = "gridDashboardItem1"
-            Me.gridDashboardItem1.DataItemRepository.Clear()
-            Me.gridDashboardItem1.DataItemRepository.Add(measure3, "DataItem1")
-            Me.gridDashboardItem1.DataItemRepository.Add(dimension5, "DataItem0")
-            Me.gridDashboardItem1.DataSource = Me.dashboardExtractDataSource1
-            Me.gridDashboardItem1.InteractivityOptions.IgnoreMasterFilters = False
-            Me.gridDashboardItem1.Name = "Grid 1"
-            Me.gridDashboardItem1.ShowCaption = False
+            measure4.DataMember = "RegularPrice"
+            card1.DeltaOptions.ResultIndicationThreshold = New Decimal(New Integer() { 5, 0, 0, 0})
+            card1.LayoutDeltaOptions.ResultIndicationThreshold = New Decimal(New Integer() { 5, 0, 0, 0})
+            cardCompactLayoutTemplate1.BottomSubValue1.DimensionIndex = 0
+            cardCompactLayoutTemplate1.BottomSubValue1.ValueType = DevExpress.DashboardCommon.CardRowDataElementType.AbsoluteVariation
+            cardCompactLayoutTemplate1.BottomSubValue1.Visible = True
+            cardCompactLayoutTemplate1.BottomSubValue2.DimensionIndex = 0
+            cardCompactLayoutTemplate1.BottomSubValue2.ValueType = DevExpress.DashboardCommon.CardRowDataElementType.PercentVariation
+            cardCompactLayoutTemplate1.BottomSubValue2.Visible = True
+            cardCompactLayoutTemplate1.BottomValue.DimensionIndex = 0
+            cardCompactLayoutTemplate1.BottomValue.ValueType = DevExpress.DashboardCommon.CardRowDataElementType.ActualValue
+            cardCompactLayoutTemplate1.BottomValue.Visible = True
+            cardCompactLayoutTemplate1.DeltaIndicator.Visible = True
+            cardCompactLayoutTemplate1.MainValue.DimensionIndex = 0
+            cardCompactLayoutTemplate1.MainValue.ValueType = DevExpress.DashboardCommon.CardRowDataElementType.Title
+            cardCompactLayoutTemplate1.MainValue.Visible = True
+            cardCompactLayoutTemplate1.MaxWidth = 150
+            cardCompactLayoutTemplate1.MinWidth = 125
+            cardCompactLayoutTemplate1.Sparkline.Visible = True
+            cardCompactLayoutTemplate1.SubValue.DimensionIndex = 0
+            cardCompactLayoutTemplate1.SubValue.ValueType = DevExpress.DashboardCommon.CardRowDataElementType.Subtitle
+            cardCompactLayoutTemplate1.SubValue.Visible = True
+            card1.LayoutTemplate = cardCompactLayoutTemplate1
+            card1.AddDataItem("ActualValue", measure3)
+            card1.AddDataItem("TargetValue", measure4)
+            Me.cardDashboardItem1.Cards.AddRange(New DevExpress.DashboardCommon.Card() { card1})
+            Me.cardDashboardItem1.ComponentName = "cardDashboardItem1"
+            dimension4.DataMember = "SalesPerson"
+            dimension5.DataMember = "OrderDate"
+            dimension5.DateTimeGroupInterval = DevExpress.DashboardCommon.DateTimeGroupInterval.MonthYear
+            Me.cardDashboardItem1.DataItemRepository.Clear()
+            Me.cardDashboardItem1.DataItemRepository.Add(measure4, "DataItem0")
+            Me.cardDashboardItem1.DataItemRepository.Add(measure3, "DataItem1")
+            Me.cardDashboardItem1.DataItemRepository.Add(dimension4, "DataItem3")
+            Me.cardDashboardItem1.DataItemRepository.Add(dimension5, "DataItem2")
+            Me.cardDashboardItem1.DataSource = Me.dashboardExtractDataSource1
+            Me.cardDashboardItem1.InteractivityOptions.IgnoreMasterFilters = False
+            Me.cardDashboardItem1.Name = "Cards 1"
+            Me.cardDashboardItem1.SeriesDimensions.AddRange(New DevExpress.DashboardCommon.Dimension() { dimension4})
+            Me.cardDashboardItem1.ShowCaption = True
+            Me.cardDashboardItem1.SparklineArgument = dimension5
             ' 
             ' SalesDashboard
             ' 
             Me.DataSources.AddRange(New DevExpress.DashboardCommon.IDashboardDataSource() { Me.dashboardExtractDataSource1})
-            Me.Items.AddRange(New DevExpress.DashboardCommon.DashboardItem() { Me.pivotDashboardItem1, Me.gridDashboardItem1, Me.chartDashboardItem1})
-            dashboardLayoutItem1.DashboardItem = Me.pivotDashboardItem1
-            dashboardLayoutItem1.Weight = 49.921752738654149R
-            dashboardLayoutItem2.DashboardItem = Me.chartDashboardItem1
-            dashboardLayoutItem2.Weight = 49.876543209876544R
-            dashboardLayoutItem3.DashboardItem = Me.gridDashboardItem1
-            dashboardLayoutItem3.Weight = 50.123456790123456R
-            dashboardLayoutGroup2.ChildNodes.AddRange(New DevExpress.DashboardCommon.DashboardLayoutNode() { dashboardLayoutItem2, dashboardLayoutItem3})
+            Me.Items.AddRange(New DevExpress.DashboardCommon.DashboardItem() { Me.pivotDashboardItem1, Me.chartDashboardItem1, Me.cardDashboardItem1})
+            dashboardLayoutItem1.DashboardItem = Me.chartDashboardItem1
+            dashboardLayoutItem1.Weight = 50R
+            dashboardLayoutItem2.DashboardItem = Me.pivotDashboardItem1
+            dashboardLayoutItem2.Weight = 50R
+            dashboardLayoutGroup2.ChildNodes.AddRange(New DevExpress.DashboardCommon.DashboardLayoutNode() { dashboardLayoutItem1, dashboardLayoutItem2})
             dashboardLayoutGroup2.DashboardItem = Nothing
-            dashboardLayoutGroup2.Orientation = DevExpress.DashboardCommon.DashboardLayoutGroupOrientation.Vertical
-            dashboardLayoutGroup2.Weight = 50.078247261345851R
-            dashboardLayoutGroup1.ChildNodes.AddRange(New DevExpress.DashboardCommon.DashboardLayoutNode() { dashboardLayoutItem1, dashboardLayoutGroup2})
+            dashboardLayoutGroup2.Weight = 50R
+            dashboardLayoutItem3.DashboardItem = Me.cardDashboardItem1
+            dashboardLayoutItem3.Weight = 50R
+            dashboardLayoutGroup1.ChildNodes.AddRange(New DevExpress.DashboardCommon.DashboardLayoutNode() { dashboardLayoutGroup2, dashboardLayoutItem3})
             dashboardLayoutGroup1.DashboardItem = Nothing
+            dashboardLayoutGroup1.Orientation = DevExpress.DashboardCommon.DashboardLayoutGroupOrientation.Vertical
             Me.LayoutRoot = dashboardLayoutGroup1
             Me.Title.Text = "Dashboard"
             CType(dimension1, System.ComponentModel.ISupportInitialize).EndInit()
             CType(measure1, System.ComponentModel.ISupportInitialize).EndInit()
             CType(dimension2, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(dimension3, System.ComponentModel.ISupportInitialize).EndInit()
             CType(Me.pivotDashboardItem1, System.ComponentModel.ISupportInitialize).EndInit()
             CType(Me.dashboardExtractDataSource1, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(dimension4, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(dimension3, System.ComponentModel.ISupportInitialize).EndInit()
             CType(measure2, System.ComponentModel.ISupportInitialize).EndInit()
             CType(Me.chartDashboardItem1, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(dimension5, System.ComponentModel.ISupportInitialize).EndInit()
             CType(measure3, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(Me.gridDashboardItem1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(measure4, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(dimension4, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(dimension5, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.cardDashboardItem1, System.ComponentModel.ISupportInitialize).EndInit()
             CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
 
         End Sub
@@ -166,7 +198,7 @@
 
         Private dashboardExtractDataSource1 As DevExpress.DashboardCommon.DashboardExtractDataSource
         Private pivotDashboardItem1 As DevExpress.DashboardCommon.PivotDashboardItem
-        Private gridDashboardItem1 As DevExpress.DashboardCommon.GridDashboardItem
         Private chartDashboardItem1 As DevExpress.DashboardCommon.ChartDashboardItem
+        Private cardDashboardItem1 As DevExpress.DashboardCommon.CardDashboardItem
     End Class
 End Namespace
